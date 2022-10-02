052e300eca214f167673705dea004e5537ae46ba
Augusto Lopes <augustolfp@gmail.com>
2022-10-02 16:58:16 -0300

    feat(Database): Add seed to populate TacoTable with food properties

---
+++ prisma/seed.ts
@@ -0,0 +1,16 @@
+import foodList from './finalFoodList.json';
+import { PrismaClient } from '@prisma/client';
+
+const prisma = new PrismaClient();
+
+async function main() {
+  for (const food of foodList) {
+    await prisma.tacoTable.create({
+      data: food
+    });
+  }
+}
+
+main().catch(() => {
+  process.exit(1);
+});
