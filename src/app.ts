07f544f9c1592ccdab3da4c8c382ddb71586affe
Augusto Lopes <augustolfp@gmail.com>
2022-10-01 21:09:56 -0300

    style(Linter): Configure ESLint and prettier on project

--- src/app.ts
+++ src/app.ts
@@ -1,7 +1,7 @@
-import express from "express";
-import "express-async-errors";
-import cors from "cors";
-import dotenv from "dotenv";
+import express from 'express';
+import 'express-async-errors';
+import cors from 'cors';
+import dotenv from 'dotenv';
 
 dotenv.config();
 
