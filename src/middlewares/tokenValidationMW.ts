15f6cd4a6fd02cbef9b232c56401386b08ffe4c1
Augusto Lopes <augustolfp@gmail.com>
2022-10-03 14:20:44 -0300

    feat(JWT-auth): Implement JWT token authorization middleware

---
+++ src/middlewares/tokenValidationMW.ts
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+import { NextFunction, Request, Response } from 'express';
+
+export default function tokenValidationMW(
+  req: Request,
+  res: Response,
+  next: NextFunction
+) {
+  // eslint-disable-next-line @typescript-eslint/no-non-null-assertion
+  const jwtKey: string = process.env.JWT_SECRET!;
+  const { authorization } = req.headers;
+  const token = authorization?.replace('Bearer ', '');
+
+  if (!token) {
+    return res.status(401).send('Formato de token inválido!');
+  }
+
+  try {
+    const tokenEmbeddedData = jwt.verify(token, jwtKey);
+    res.locals.userData = tokenEmbeddedData;
+    next();
+  } catch (error) {
+    return res.status(401).send('Token inválido!');
+  }
+}
