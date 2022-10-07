eb042968b8a4b0dfb62270a4731bc0d79ba189f9
Augusto Lopes <augustolfp@gmail.com>
2022-10-07 15:52:15 -0300

    fix(Sign-in test): Add type check to jwt payload

--- tests/integration/factories/tokenFactory.ts
+++ tests/integration/factories/tokenFactory.ts
@@ -1,12 +1,14 @@
 import jwt from 'jsonwebtoken';
 import { Users } from '@prisma/client';
 
+// eslint-disable-next-line @typescript-eslint/no-non-null-assertion
 const jwtKey: string = process.env.JWT_SECRET!;
 
 export function validateToken(token: string, user: Users) {
   const payload = jwt.verify(token, jwtKey);
 
   if (
+    typeof payload !== 'string' &&
     payload.email === user.email &&
     payload.name === user.name &&
     payload.userId === user.id
