73bccf7d5fd67ec6831fac0a3c38a83e07bad95e
Augusto Lopes <augustolfp@gmail.com>
2022-11-06 17:36:03 -0300

    feat(Add ingredients): Add function to truncate received numbers

--- src/services/ingredientService.ts
+++ src/services/ingredientService.ts
@@ -2,5 +2,18 @@ import * as ingredientRepo from '../repositories/ingredientRepository';
 import { IIngredientData } from '../types/ingredientTypes';
 
 export async function create(ingredient: IIngredientData) {
-  return await ingredientRepo.create(ingredient);
+  function truncNumber(number: number) {
+    return Math.round(number * 10) / 10;
+  }
+
+  const truncIng = {
+    ...ingredient,
+    weight: truncNumber(ingredient.weight),
+    carbs: truncNumber(ingredient.carbs),
+    fats: truncNumber(ingredient.fats),
+    proteins: truncNumber(ingredient.proteins),
+    kcals: truncNumber(ingredient.kcals)
+  };
+
+  return await ingredientRepo.create(truncIng);
 }
