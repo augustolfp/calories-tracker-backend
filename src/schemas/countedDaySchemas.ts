ed31d64c860999a79c9727c33fad4bde948fcf58
Augusto Lopes <augustolfp@gmail.com>
2022-11-06 16:53:21 -0300

    feat(Add counted day): Add rule to allow only integer values of calories and proteins target

--- src/schemas/countedDaySchemas.ts
+++ src/schemas/countedDaySchemas.ts
@@ -7,6 +7,6 @@ const Joi = coreJoi.extend(joiDate) as typeof coreJoi;
 export const countedDaySchema = Joi.object<Omit<ICountedDayData, 'userId'>>({
   day: Joi.date().format('YYYY-MM-DD').required(),
   notes: Joi.string().allow('').optional(),
-  caloriesTarget: Joi.number().required(),
-  proteinsTarget: Joi.number().required()
+  caloriesTarget: Joi.number().integer().required(),
+  proteinsTarget: Joi.number().integer().required()
 });
