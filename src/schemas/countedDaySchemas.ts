ff96a09d9b94ed01f7210263a09f287b8f291791
Augusto Lopes <augustolfp@gmail.com>
2022-11-06 15:37:27 -0300

    fix(data validation): Change meal and day schemas to allow empty string

--- src/schemas/countedDaySchemas.ts
+++ src/schemas/countedDaySchemas.ts
@@ -3,7 +3,7 @@ import { ICountedDayData } from '../types/countedDayType';
 
 export const countedDaySchema = Joi.object<Omit<ICountedDayData, 'userId'>>({
   day: Joi.string().required(),
-  notes: Joi.string().optional(),
+  notes: Joi.string().allow('').optional(),
   caloriesTarget: Joi.number().required(),
   proteinsTarget: Joi.number().required()
 });
