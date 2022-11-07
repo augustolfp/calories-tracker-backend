0b1c649366a00fb908a7b7fbcd9688b655f02bf7
Augusto Lopes <augustolfp@gmail.com>
2022-11-07 18:50:29 -0300

    fix(Date format): Change date format to ISO

--- src/schemas/countedDaySchemas.ts
+++ src/schemas/countedDaySchemas.ts
@@ -5,7 +5,7 @@ import { ICountedDayData } from '../types/countedDayType';
 const Joi = coreJoi.extend(joiDate) as typeof coreJoi;
 
 export const countedDaySchema = Joi.object<Omit<ICountedDayData, 'userId'>>({
-  day: Joi.date().format('YYYY-MM-DD').required(),
+  day: Joi.date().iso().required(),
   notes: Joi.string().allow('').optional(),
   caloriesTarget: Joi.number().integer().required(),
   proteinsTarget: Joi.number().integer().required()
