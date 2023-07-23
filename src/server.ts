d839d35d705f102065b7566734b8c3bbf18590c9
augustolfp <augustolfp@gmail.com>
2023-07-23 19:01:04 -0300

    revert: remove ssl cert mounting

--- src/server.ts
+++ src/server.ts
@@ -1,19 +1,9 @@
 import dotenv from 'dotenv';
-import * as https from 'https';
-import * as fs from 'fs';
 import app from './app';
 
 dotenv.config();
 
-const httpsServer = https.createServer(
-  {
-    key: fs.readFileSync('/etc/letsencrypt/live/api.dietinha.co/privkey.pem'),
-    cert: fs.readFileSync('/etc/letsencrypt/live/api.dietinha.co/fullchain.pem')
-  },
-  app
-);
-
-httpsServer.listen(process.env.PORT, () => {
+app.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running at port ${process.env.PORT}`);
 });
