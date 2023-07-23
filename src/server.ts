6a10f460e6cee9727e957a58c17d90b3ff20ce1d
augustolfp <augustolfp@gmail.com>
2023-07-23 18:10:17 -0300

    build: configure ssl cert mounting

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,19 @@
 import dotenv from 'dotenv';
+import * as https from 'https';
+import * as fs from 'fs';
 import app from './app';
 
 dotenv.config();
 
-app.listen(process.env.PORT, () => {
+const httpsServer = https.createServer(
+  {
+    key: fs.readFileSync('/etc/letsencrypt/live/api.dietinha.co/privkey.pem'),
+    cert: fs.readFileSync('/etc/letsencrypt/live/api.dietinha.co/fullchain.pem')
+  },
+  app
+);
+
+httpsServer.listen(process.env.PORT, () => {
   // eslint-disable-next-line no-console
   console.log(`Server running at port ${process.env.PORT}`);
 });
