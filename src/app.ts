6ed0658b1855257424df4087809c02df5fa3dd2e
augustolfp <augustolfp@gmail.com>
2023-09-20 17:35:05 -0300

    ci: add configurable URL_PATH_PREFIX from env variables

--- src/app.ts
+++ src/app.ts
@@ -1,12 +1,17 @@
+import dotenv from 'dotenv';
 import express from 'express';
 import 'express-async-errors';
 import cors from 'cors';
 import router from './routes/index';
 import errorHandlerMW from './middlewares/errorHandlerMW';
 
+dotenv.config();
 const app = express();
+
+const URL_PATH_PREFIX = process.env.URL_PATH_PREFIX;
+
 app.use(express.json(), cors());
-app.use(router);
+app.use(URL_PATH_PREFIX, router);
 app.use(errorHandlerMW);
 
 export default app;
